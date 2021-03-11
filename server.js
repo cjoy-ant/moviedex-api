929e595b7e34e3510d9a1e0a992a2ad337dc5561
Christine Antonio <christinejm.antonio@gmail.com>
2021-03-11 14:31:51 -0800

    GET /movie

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 const app = express();
 const PORT = 8000;
-const movies = require("./movies-data-small.json");
+const MOVIES = require("./movies-data-small.json");
 
 app.use(morgan("dev"));
 app.use(helmet());
@@ -16,12 +16,44 @@ app.use(validateBearerToken);
 app.get("/movie", handleGetMovie);
 
 function validateBearerToken(req, res, next) {
+  const authToken = req.get("Authorization");
   const apiToken = process.env.API_TOKEN;
-  // next();
+
+  if (!authToken || authToken.split(" ")[1] !== apiToken) {
+    return res.status(401).json({ error: "Unauthorized Request" });
+  }
+
+  next();
 }
 
 function handleGetMovie(req, res) {
   const { genre, country, avg_vote } = req.query;
+  let response = MOVIES;
+
+  // search whether genre includes a specified string
+  // case insensitive
+  if (genre) {
+    response = response.filter((movie) =>
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+
+  // search whether country includes a specified string
+  // case insensitive
+  if (country) {
+    response = response.filter((movie) =>
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    );
+  }
+
+  // search for moves with avg_vote >= supplied number
+  if (avg_vote) {
+    response = response.filter(
+      (movie) => Number(movie.avg_vote) >= Number(avg_vote)
+    );
+  }
+
+  res.json(response);
 }
 
 app.listen(PORT, () =>
